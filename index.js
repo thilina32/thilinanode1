cbc600590b9f5571c281f04fac926fbad9be11e8
Thilina kavishan <113406436+thilina32@users.noreply.github.com>
2024-05-06 19:15:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,5 +16,42 @@ const db = admin.database();
 
 
 db.ref('id/').on("value", function(snapshot) {
+    const data2 = snapshot.val();
+    const now = Date.now();
+    const filePath = now + "video.mp4";
+    ytdl(data2['url'])
+    .pipe(fs.createWriteStream(filePath))
+    .on("finish", () => {
+        console.log("Video downloaded successfully.");
+
+        // Upload video to Firebase Storage
+        const fileName =  now+"video.mp4";
+        const destination = `videos/${fileName}`;
+
+        bucket.upload(fileName, { destination })
+        .then(() => {
+            console.log("Video uploaded to Firebase Storage successfully.");
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                  console.error("Error deleting file:", err);
+                } else {
+                  console.log("File deleted successfully:", filePath);
+                }
+              });
+        })
+        .catch((error) => {
+            console.error("Error uploading video to Firebase Storage:", error);
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                  console.error("Error deleting file:", err);
+                } else {
+                  console.log("File deleted successfully:", filePath);
+                }
+            });
+        });
+    })
+    .on("error", (error) => {
+        console.error("Error downloading video:", error);
+    });
     console.log(snapshot.val());
 });
